fe2cfb076353b44749af2eee0127bb972a71432d
MartynDeCitium <149655612+MartynDeCitium@users.noreply.github.com>
2023-11-30 12:48:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import width
 import streamlit as st
 import random
 
@@ -7,105 +8,78 @@ def show_main_page():
     st.subheader('Исследование выпрямительных устройств')
 
     # Добавляем изображение перед остальным кодом
-    st.image("однополупериодный выпрямитель.jpg", 
-             caption="Рисунок 1. Схема однополупериодного выпрямителя", use_column_width=True)
+    st.image("однополупериодный выпрямитель.jpg", caption="Рисунок 1. Схема однополупериодного выпрямителя", use_column_width=True)
 
-    Uinput = st.number_input('Введите U2 тр-ра', min_value=10, max_value=20, value="min", step=1)
-    st.write('U2 тр-ра = ', Uinput, ' В')
+    Uinput = st.number_input('Введите напряжение вторичной обмотки трансформатора (U2)', min_value=10, max_value=20, value="min", step=1)
+    st.write('U2 трансформатора = ', Uinput, ' В')
     Udiod = 0.45 * Uinput
 
-    RLoad = st.slider("Сопротивление нагрузки", 1.0, 5.0, 1.5)
-    st.write("Сопротивление нагрузки", RLoad, 'Ом')
+    RLoad = st.slider("Введите сопротивление нагрузки", 1.0, 5.0, 1.5)
+    st.write("Сопротивление нагрузки (Rн)", RLoad, 'Ом')
     Rvn = 0.2
     ILoad = Udiod / (RLoad + Rvn)
     st.write("Ток нагрузки", ILoad, 'А')
     st.write("Напряжение на нагрузке", RLoad * ILoad, 'В')
-    idx = random.randint(1, 100)
-    st.write("Рандомное число", idx)
-
     st.write("---")
-
-    col1, col2 = st.columns(2)
-    RLoad1 = col1.slider("Сопротивление нагрузки", 0.01, 5.0, 1.0)
-    col1.write("Сопротивление нагрузки")
 
 # Функция для отображения страницы с однофазной мостовой
 def show_single_phase_bridge_page():
     st.title('Однофазная мостовая схема выпрямления')
 
-    Uinput = st.number_input('Введите U2 тр-ра', min_value=10, max_value=20, value="min", step=1)
-    st.write('U2 тр-ра = ', Uinput, ' В')
+    # Добавляем изображение перед остальным кодом
+    st.image("мостовой выпрямитель.jpg", caption="Рисунок 2. Схема мостового выпрямителя", use_column_width=True)
+
+    Uinput = st.number_input('Введите напряжение вторичной обмотки трансформатора (U2)', min_value=10, max_value=20, value="min", step=1)
+    st.write('U2 трансформатора = ', Uinput, ' В')
     Udiod = 0.9 * Uinput
 
-    RLoad = st.slider("Сопротивление нагрузки", 1.0, 5.0, 1.5)
-    st.write("Сопротивление нагрузки", RLoad, 'Ом')
+    RLoad = st.slider("Введите сопротивление нагрузки", 1.0, 5.0, 1.5)
+    st.write("Сопротивление нагрузки (Rн)", RLoad, 'Ом')
     Rvn = 0.2
     ILoad = Udiod / (RLoad + Rvn)
     st.write("Ток нагрузки", ILoad, 'А')
     st.write("Напряжение на нагрузке", RLoad * ILoad, 'В')
-    idx = random.randint(1, 100)
-    st.write("Рандомное число", idx)
-
     st.write("---")
-
-    col1, col2 = st.columns(2)
-    RLoad1 = col1.slider("Сопротивление нагрузки", 0.01, 5.0, 1.0)
-    col1.write("Сопротивление нагрузки")
-    col2.write('Рисунок 2. Схема однофазного мостового выпрямителя')
-    col2.image("мостовой выпрямитель.jpg")
 
 # Функция для отображения страницы с трехфазной 1-полупериодной
 def show_three_phase_single_half_wave_page():
     st.title('Трехфазная однополупериодная схема выпрямления')
 
-    Uinput = st.number_input('Введите U2 тр-ра', min_value=10, max_value=20, value="min", step=1)
-    st.write('U2 тр-ра = ', Uinput, ' В')
-    Udiod = 0.45 * Uinput
+    st.image("3-фазная одно.jpg", caption="Рисунок 3. Схема однополупериодного трехфазного выпрямителя", use_column_width=True)
+    Uinput = st.number_input('Введите напряжение вторичной обмотки трансформатора (U2)', min_value=10, max_value=20, value="min", step=1)
+    st.write('U2 трансформатора = ', Uinput, ' В')
+    Udiod = 1.17 * Uinput
 
-    RLoad = st.slider("Сопротивление нагрузки", 1.0, 5.0, 1.5)
-    st.write("Сопротивление нагрузки", RLoad, 'Ом')
+    RLoad = st.slider("Введите сопротивление нагрузки", 1.0, 5.0, 1.5)
+    st.write("Сопротивление нагрузки (Rн)", RLoad, 'Ом')
     Rvn = 0.2
     ILoad = Udiod / (RLoad + Rvn)
     st.write("Ток нагрузки", ILoad, 'А')
     st.write("Напряжение на нагрузке", RLoad * ILoad, 'В')
-    idx = random.randint(1, 100)
-    st.write("Рандомное число", idx)
-
     st.write("---")
 
-    col1, col2 = st.columns(2)
-    RLoad1 = col1.slider("Сопротивление нагрузки", 0.01, 5.0, 1.0)
-    col1.write("Сопротивление нагрузки")
-    col2.write('Рисунок 3. Трехфазная однополупериодная схема выпрямления')
-    # col2.image("Рисунок3.jpg")
 
 # Функция для отображения страницы с трехфазной мостовой
 def show_three_phase_bridge_page():
     st.title('Трехфазная мостовая схема выпрямления')
 
-    Uinput = st.number_input('Введите U2 тр-ра', min_value=10, max_value=20, value="min", step=1)
-    st.write('U2 тр-ра = ', Uinput, ' В')
-    Udiod = 0.45 * Uinput
+    st.image("3-фазная мост.jpg", caption="Рисунок 4. Схема мостового трехфазного выпрямителя", use_column_width=True)
+    Uinput = st.number_input('Введите напряжение вторичной обмотки трансформатора (U2)', min_value=10, max_value=20, value="min", step=1)
+    st.write('U2 трансформатора = ', Uinput, ' В')
+    Udiod = 2.34 * Uinput
 
-    RLoad = st.slider("Сопротивление нагрузки", 1.0, 5.0, 1.5)
-    st.write("Сопротивление нагрузки", RLoad, 'Ом')
+    RLoad = st.slider("Введите сопротивление нагрузки", 1.0, 5.0, 1.5)
+    st.write("Сопротивление нагрузки (Rн)", RLoad, 'Ом')
     Rvn = 0.2
     ILoad = Udiod / (RLoad + Rvn)
     st.write("Ток нагрузки", ILoad, 'А')
     st.write("Напряжение на нагрузке", RLoad * ILoad, 'В')
-    idx = random.randint(1, 100)
-    st.write("Рандомное число", idx)
-
     st.write("---")
 
-    col1, col2 = st.columns(2)
-    RLoad1 = col1.slider("Сопротивление нагрузки", 0.01, 5.0, 1.0)
-    col1.write("Сопротивление нагрузки")
-    col2.write('Рисунок 4. Трехфазная мостовая схема выпрямления')
-    # col2.image("Рисунок4.jpg")
-
-    # Основная часть программы
-page = st.sidebar.selectbox("Выберите страницу", ["Однофазная 1-полупериодная", "Однофазная мостовая", "Трехфазная 1-полупериодная", "Трехфазная мостовая"])
+# Основная часть программы
+st.sidebar.title("Лабораторная работа №2")
+st.sidebar.subheader("Исследование выпрямительных устройств")
+page = st.sidebar.selectbox("Выберите тип выпрямителя", ["Однофазная 1-полупериодная", "Однофазная мостовая", "Трехфазная 1-полупериодная", "Трехфазная мостовая"])
 
 if page == "Однофазная 1-полупериодная":
     show_main_page()
